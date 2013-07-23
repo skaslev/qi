7d666d1b94fa69c601e4941ef1b3ccad1f7c2cc3
Slavomir Kaslev <slavomir.kaslev@gmail.com>
2013-07-22 23:13:04 -0700

    Renaming.

--- src/Qi.hs
+++ src/Qi.hs
@@ -12,31 +12,29 @@ a # b = genericIndex a b
 sieve :: [N] -> [N]
 sieve (x:xs) = x : sieve [y | y <- xs, y `mod` x /= 0]
 
-primes :: [N]
-primes = sieve [2..]
+prime :: [N]
+prime = sieve [2..]
 
 data Q a b
-  where MkQ :: a -> b -> Q a b
+  where Qa :: a -> b -> Q a b
   deriving Show
 
 q :: N -> Q N C
-q n = MkQ (primes # n) (1 :+ 0)
+q n = Qa (prime # n) (1 :+ 0)
 
 data Zero a b
-  where MkZero :: a -> b -> Zero a b
+  where Zero :: a -> b -> Zero a b
   deriving Show
 
 zero :: Zero N C
-zero = MkZero 0 (0 :+ 0)
+zero = Zero 0 (0 :+ 0)
 
 data One a b
-  where MkOne :: a -> b -> One a b
+  where One :: a -> b -> One a b
   deriving Show
 
 one :: One N C
-one = MkOne 1 (1 :+ 0)
+one = One 1 (1 :+ 0)
 
-
-
-quants :: [N]
-quants = 0 : 1 : primes
+quant :: [N]
+quant = 0 : 1 : prime
