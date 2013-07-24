e88de7d963aef987cd778838c555d03a070de8fa
Slavomir Kaslev <slavomir.kaslev@gmail.com>
2013-07-24 00:47:49 -0700

    .

--- src/Qi.hs
+++ src/Qi.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GADTs #-}
 import Data.Complex
 import Data.List hiding ((!!), replicate)
+import Data.List.Utils
 import Prelude hiding ((!!), replicate)
 
 (!!) = genericIndex
@@ -13,15 +14,15 @@ type C = Complex Float
 sieve :: [N] -> [N]
 sieve (x:xs) = x : sieve [y | y <- xs, y `mod` x /= 0]
 
-prime :: [N]
-prime = sieve [2..]
+primes :: [N]
+primes = sieve [2..]
 
 data Q a b
   where Qi :: a -> b -> Q a b
   deriving Show
 
 q :: N -> Q N C
-q i = Qi (prime !! i) (1 :+ 0)
+q i = Qi (primes !! i) (1 :+ 0)
 
 data Vacuum a b
   where Vacuum :: a -> b -> Vacuum a b
@@ -37,8 +38,8 @@ data One a b
 one :: One N C
 one = One 1 (1 :+ 0)
 
-quant :: [N]
-quant = 0 : 1 : prime
+quants :: [N]
+quants = 0 : 1 : primes
 
 d :: Num a => [a] -> [a]
 d (x:y:xs) = (y-x) : d (y:xs)
@@ -57,25 +58,20 @@ data Nat a b
 factor :: N -> [N]
 factor 0 = []
 factor 1 = []
-factor n =
-  (\(p:ps) ->
-    let k = dlog p n
-    in (replicate k p) ++ (factor (n `div` (p^k)))) $
-  dropWhile (\p -> n `mod` p /= 0) $
-  takeWhile (<= n) $
-  prime
-
--- p <- prime
-dlog :: N -> N -> N
-dlog p n | r /= 0 = 0
-         | otherwise = 1 + dlog p q
+factor n = replicate k p ++ factor (n `div` (p^k))
   where
-    (q,r) = divMod n p
+    p = head $
+        dropWhile (\p -> n `mod` p /= 0) $
+        takeWhile (<= n) $
+        primes
+    k = dlog p n
+    dlog p n | r /= 0 = 0
+             | otherwise = 1 + dlog p q
+      where
+        (q,r) = divMod n p
 
 --factor :: N -> Cantor
 --factor = epsilon is_factor
---factor :: N -> Dist  -- N -> [N]
---factor = [f i | i <-
 
 type Cantor = N -> Bool
 type Pred = Cantor -> Bool
